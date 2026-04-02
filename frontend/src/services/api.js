7af46badb79a158a13722ab8af215e2f868c673a
justhuuthang <95196568+justhuuthang@users.noreply.github.com>
2026-04-02 22:59:26 +0700

    connect to be

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const API = import.meta.env.VITE_API_URL;
 
-export const getLevels = () => axios.get(`${API}/levels`);
-export const getLektions = (id) => axios.get(`${API}/lektions/${id}`);
-export const getVocab = (id) => axios.get(`${API}/vocab/${id}`);
+export const getLevels = () => axios.get(`${API}/lessons/levels`);
+export const getLektions = (id) => axios.get(`${API}/lessons/lektions/${id}`);
+export const getVocab = (id) => axios.get(`${API}/lessons/vocab/${id}`);
